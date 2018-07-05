7cb7451ec5e36ca427ff1ede5bfc8e40006cffd8
Jordan Palmer <jordan.s.palmer@outlook.com>
2018-07-05 16:50:59 -0700

    Pull workshop information from database instead of dummy json file

--- app/controllers/tao_controller.rb
+++ app/controllers/tao_controller.rb
@@ -1,10 +1,27 @@
 class TaoController < ApplicationController
   def workshops
-    render :json => File.read(Rails.root.join("app/views/tao/dummy.json"))
+   #render :json => File.read(Rails.root.join("app/views/tao/dummy.json"))
+	  @workshops = []
+	  Workshop.all.each do |w|
+	  	workshop = {:id => w.id, :name => w.name, :registrants => []}
+	  	w.registrations.each do |r|
+	  		a = r.attendee
+	  		workshop[:registrants].push({:id => a.id, :name => a.name, :attended => r.attended})
+	  	end
+	  	@workshops.push(workshop)
+	  end
+	  render :json => @workshops
   end
 
   def attend
     # render :json => {status: "fail"}
-    render :json => {status: "success"}
+    r = Registration.where(:attendee_id => params[:attendee_id], :workshop_id => params[:workshop_id]).first
+    r.attended = true;
+    
+    if r.save
+    	render :json => {status: "success"}
+    else
+    	render :json => {status: "failure"}
+    end
   end
 end

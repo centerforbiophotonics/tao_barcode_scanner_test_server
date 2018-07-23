919acc812becbbdf7496488aa3b1ac8deb96b57e
Jordan Palmer <jordan.s.palmer@outlook.com>
2018-07-23 15:35:23 -0700

    Remove simulated error generation from controller

--- app/controllers/tao_controller.rb
+++ app/controllers/tao_controller.rb
@@ -1,6 +1,5 @@
 class TaoController < ApplicationController
   def workshops
-   #render :json => File.read(Rails.root.join("app/views/tao/dummy.json"))
 	  @workshops = []
 	  Workshop.all.each do |w|
 	  	workshop = {:id => w.id, :name => w.name, :registrants => []}
@@ -10,29 +9,19 @@ class TaoController < ApplicationController
 	  	end
 	  	@workshops.push(workshop)
 	  end
-	  number = rand(10)
-	  if (number <= 4 && number >= 0)
-	  	render json: @resource, status: 500
-	  else
-	  	render :json => @workshops
-	  end
+	  render :json => @workshops
   end
 
   def attend
-    # render :json => {status: "fail"}
     a = Attendee.where(:kerberos => params[:attendee_id]).first
     r = Registration.where(:attendee_id => a.id, :workshop_id => params[:workshop_id]).first
     r.attended = true;
     number = rand(10)
     puts number
-    if number <= 8 && number >= 0
-	    if r.save
-	    	render :json => {status: "success"}
-	    else
-	    	render :json => {status: "failure"}
-	    end
-	else
-		 render json: @resource, status: 500
-	end
+    if r.save
+    	render :json => {status: "success"}
+    else
+    	render :json => {status: "failure"}
+    end
   end
 end
